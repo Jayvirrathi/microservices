a723135232d889d60986c19a879399c225948417
Jayvir Rathi <jayvirrathi@gmail.com>
2020-08-15 22:28:41 +0530

    refactor default data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let order = connect("127.0.0.1:27017/order");
 
 book.books.insertMany([
   {
-    _id: "5f380bd326509c2b381f8c5c",
+    _id: ObjectId("5f380bd326509c2b381f8c5c"),
     title: "Think and Grow Rich",
     author: "Napoleon Hill",
     numberPages: 250,
@@ -15,7 +15,7 @@ book.books.insertMany([
 
 customer.customers.insertMany([
   {
-    _id: "5f380bf52592f6162830adfe",
+    _id: ObjectId("5f380bf52592f6162830adfe"),
     name: "Jack Ryan",
     age: 29,
     address: "Newyork",
@@ -25,9 +25,9 @@ customer.customers.insertMany([
 
 order.orders.insertMany([
   {
-    _id: "5f380df62de32327548f7481",
-    customerId: "5f380bf52592f6162830adfe",
-    bookId: "5f380bd326509c2b381f8c5c",
+    _id: ObjectId("5f380df62de32327548f7481"),
+    customerId: ObjectId("5f380bf52592f6162830adfe"),
+    bookId: ObjectId("5f380bd326509c2b381f8c5c"),
     initialDate: "2020-08-15T00:00:00.000Z",
     deliveryDate: "2020-08-16T00:00:00.000Z",
     __v: 0,
